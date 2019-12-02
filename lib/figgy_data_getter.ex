25feea1a8c39eb362212153ac95776ba58902d32
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2019-12-02 15:32:17 -0500

    adds json parsing methods

--- lib/figgy_data_getter.ex
+++ lib/figgy_data_getter.ex
@@ -15,4 +15,23 @@ defmodule FiggyDataGetter do
   def hello do
     :world
   end
+
+  def get_figgy_catalog do
+    get_url("https://figgy.princeton.edu/catalog.json")
+    |> parse_json
+  end
+
+  def parse_json(json_string) when is_binary(json_string) do
+    json_string
+    |> Jason.decode
+    |> parse_json
+  end
+
+  def parse_json({:ok, json = %{}}), do: json
+
+  def get_url(url) do
+    url
+    |> HTTPoison.get!([], [ ssl: [versions: [:'tlsv1.2']] ])
+    |> Map.get(:body)
+  end
 end
